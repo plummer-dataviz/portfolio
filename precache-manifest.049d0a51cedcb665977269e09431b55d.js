c5352ebbbda6d24bfdd3b18e3a033d7cab31ecc4
Matthew Plummer <plummat@gmail.com>
2020-05-22 10:33:34 -0400

    Updates

---
+++ precache-manifest.049d0a51cedcb665977269e09431b55d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba79ed79429ada76f58bd72ffc7c0d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5082645151b02967576",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7886b0278f7e074597ac",
+    "url": "/static/js/2.27547062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27547062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5082645151b02967576",
+    "url": "/static/js/main.c7e6524d.chunk.js"
+  },
+  {
+    "revision": "3dc5376b37b581915f77",
+    "url": "/static/js/runtime-main.01d8ddf0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
