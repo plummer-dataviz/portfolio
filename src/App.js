61a2cb9e52c20f01a0a561492e8ef5dfb672784b
Matthew Plummer <plummat@gmail.com>
2020-05-22 10:33:20 -0400

    port.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>Dataviz Portfolio</h3>
+
+    <h5><a href="https://plummer-dataviz.github.io/gfm-viz/">GoFundMe: A Narative Deep Dive</a></h5>
+    <h5><a href="https://plummer-dataviz.github.io/nyc-c19/">Coronavirus in NYC by Borough in March</a></h5>
+
     </div>
   );
 }
