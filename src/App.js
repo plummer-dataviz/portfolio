b48d413a29746abb0c6b935c60ebabfeeafff2ca
Matthew Plummer <plummat@gmail.com>
2020-05-22 11:20:10 -0400

    add homepaghe.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h3>Dataviz Portfolio</h3>
+      <h3>Dataviz Portfolio</h3>
 
-    <h5><a href="https://plummer-dataviz.github.io/gfm-viz/">GoFundMe: A Narative Deep Dive</a></h5>
-    <h5><a href="https://plummer-dataviz.github.io/nyc-c19/">Coronavirus in NYC by Borough in March</a></h5>
-
+      <h5>
+        <a href="https://plummer-dataviz.github.io/gfm-viz/">
+          GoFundMe: A Narative Deep Dive
+        </a>
+      </h5>
+      <h5>
+        <a href="https://plummer-dataviz.github.io/nyc-c19/">
+          Coronavirus in NYC by Borough in March
+        </a>
+      </h5>
+      <h5>
+        <a
+          href="https://docs.google.com/document/d/1wVjX2hFWqiSka6wPSIsMsdU0la-E16JQ0LW5kfsBB-c/edit?usp=sharing
+"
+        >
+          Reflections
+        </a>
+      </h5>
     </div>
   );
 }
